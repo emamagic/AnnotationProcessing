210184bc026454720a807b9ee659bbbfb1ba1479
developer developer <developermagic7@gmail.com>
2021-03-31 10:16:39 -0700

    createBuilder

--- app/src/main/java/com/emamagic/builderannotation/MainActivity.java
+++ app/src/main/java/com/emamagic/builderannotation/MainActivity.java
@@ -3,12 +3,24 @@ package com.emamagic.builderannotation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Person person = new Person.Builder()
+                            .setName("ali")
+                            .setFamily("rezaii")
+                            .setAge(12)
+                            .build();
+
+        Log.e(TAG, "onCreate: "+ person.toString());
+
     }
 }
--- app/src/main/java/com/emamagic/builderannotation/Person.java
+++ app/src/main/java/com/emamagic/builderannotation/Person.java
@@ -2,37 +2,50 @@ package com.emamagic.builderannotation;
 
 public class Person {
 
-    String name;
-    String family;
-    int age;
+    private final String name;
+    private final String family;
+    private final int age;
 
-    public Person(String name, String family, int age) {
+    private Person(String name, String family, int age) {
         this.name = name;
         this.family = family;
         this.age = age;
     }
 
-    public String getName() {
-        return name;
+    public static class Builder {
+        private String name;
+        private String family;
+        private int age;
+
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setFamily(String family) {
+            this.family = family;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(name, family, age);
+        }
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFamily() {
-        return family;
-    }
-
-    public void setFamily(String family) {
-        this.family = family;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", family='" + family + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
+
+
