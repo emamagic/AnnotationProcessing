b139b691db6da063be5a0f7a9f1c06c98d803c8c
developer developer <developermagic7@gmail.com>
2021-03-31 09:56:30 -0700

    problem

---
+++ app/src/main/java/com/emamagic/builderannotation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.emamagic.builderannotation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ codgen/src/main/java/com/emamagic/codgen/Processor.java
@@ -0,0 +1,48 @@
+package com.emamagic.codgen;
+
+import com.emamagic.annotation.Builder;
+import com.squareup.javapoet.ClassName;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+
+public class Processor extends AbstractProcessor {
+
+    private Filer filer;
+    private Messager messager;
+    private Elements elementUtils;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+        filer = processingEnv.getFiler();
+        messager = processingEnv.getMessager();
+        elementUtils = processingEnv.getElementUtils();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+        return false;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return new TreeSet<>(Collections.singletonList(Builder.class.getCanonicalName()));
+    }
+}
